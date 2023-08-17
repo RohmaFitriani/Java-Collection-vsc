081ca26cfe72ed5e71e0b061f3eca111e18042af
RohmaFitriani <141742041+RohmaFitriani@users.noreply.github.com>
2023-08-17 21:41:57 +0700

    Add files via upload

---
+++ Java Collection.js
@@ -0,0 +1,20 @@
+ import java.util.*;
+
+ class latihan}
+
+    public static void main(String args){
+        //Set: HashSet, LinkedHashSet, Treeset, 
+        LinkedHashSet<String> nama = new LinkedHashSet<String>();
+
+        nama.add("Hilman"); nama.add("Endy"); nama.add("Tiga");
+
+        Iterator<String> iterator = nama.iterator();
+
+        while (iterator.hasNext()) {
+            FileSystem.out.print(iterator.next() + " ");
+        }
+
+      }
+
+    }
+
